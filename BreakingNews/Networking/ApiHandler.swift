d897aea7ed7f6f738d530373b7b6f680d20a7ead
Asmaa <asmaatarek93@gmail.com>
2021-12-24 17:32:38 +0200

    Add generic network layer for fetching data

---
+++ BreakingNews/Networking/ApiHandler.swift
@@ -0,0 +1,68 @@
+//
+//  ApiHandler.swift
+//  BreakingNews
+//
+//  Created by Asmaa Tarek on 24/12/2021.
+//
+
+import Foundation
+import Alamofire
+
+typealias NetworkResponse<T> = (Result<T, Error>) -> Void
+
+enum NetworkError: String, Error {
+    case generalError = "Something went wrong, please try again"
+}
+
+protocol ApiHandlerProtocol {
+    func fetchData<T: Decodable>(request: Requestable, mappingClass: T.Type, completion: NetworkResponse<T>?)
+}
+
+class ApiHandler: ApiHandlerProtocol {
+    
+    func fetchData<T: Decodable>(request: Requestable, mappingClass: T.Type, completion: NetworkResponse<T>?) {
+        getRequestData(request: request).validate(statusCode: 200...300).responseJSON { [weak self] response in
+            guard let self = self else { return }
+            let response = self.handleResponse(response: response, mappingClass: T.self)
+            switch response {
+            case .success(let decodedObject):
+                completion?(.success(decodedObject))
+            case .failure(let error):
+                completion?(.failure(error))
+            }
+        }
+    }
+}
+
+private extension ApiHandler {
+
+    func getRequestData(request: Requestable) -> DataRequest {
+        let requestData = AF.request(request.baseURL + request.path,
+                                     method: request.method,
+                                     parameters: request.parameters,
+                                     encoding: request.encoding,
+                                     headers: request.headers)
+    
+        return requestData
+    }
+
+    func handleResponse<T: Decodable> (response: AFDataResponse<Any>, mappingClass: T.Type) -> Swift.Result<T, Error> {
+        switch response.result {
+        case .success:
+            guard let jsonResponse = response.data else {
+                return .failure(NetworkError.generalError)
+            }
+            do {
+                let decodedObj = try JSONDecoder().decode(T.self, from: jsonResponse)
+                return .success(decodedObj)
+            } catch let error {
+                debugPrint("Error in decoding ** \n \(error.localizedDescription.description)")
+                return .failure(NetworkError.generalError)
+            }
+
+        case .failure(let error):
+            debugPrint(error.localizedDescription.description)
+            return .failure(error)
+        }
+    }
+}
---
+++ BreakingNews/Networking/NetworkConstants.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkConstants.swift
+//  BreakingNews
+//
+//  Created by Asmaa Tarek on 24/12/2021.
+//
+
+import Foundation
+
+enum NetworkConstants {
+    static let apiKey = "7842692dacd94897bf0888e015133c1c"
+    static let baseURL = "https://newsapi.org/v2/"
+}
