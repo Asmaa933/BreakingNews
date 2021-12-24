db428d66848d53a6f8e950900b19b57fd985d208
Asmaa <asmaatarek93@gmail.com>
2021-12-24 03:23:19 +0200

    Linting CustomDropDown

--- BreakingNews/Helpers/CustomUI/CustomDropDown/CustomDropDown.swift
+++ BreakingNews/Helpers/CustomUI/CustomDropDown/CustomDropDown.swift
@@ -9,10 +9,10 @@ import UIKit
 import DropDown
 
 class CustomDropDown: UITextField {
-    
+
     var didSelectItem: ((Int) -> Void)? // index
     private let dropMenu = DropDown()
-    
+
     public var dropMenuItems: [String]? {
         get {
             return dropMenu.dataSource
@@ -21,7 +21,7 @@ class CustomDropDown: UITextField {
             dropMenu.dataSource = newValue ?? []
         }
     }
-    
+
     override func awakeFromNib() {
         superview?.awakeFromNib()
         setupDropDown()
@@ -29,7 +29,7 @@ class CustomDropDown: UITextField {
 }
 
 fileprivate extension CustomDropDown {
-    
+
     func setupDropDown() {
         dropMenu.bottomOffset = CGPoint(x: 0, y: (self.bounds.height))
         dropMenu.direction = .any
@@ -39,7 +39,7 @@ fileprivate extension CustomDropDown {
             self.handleItemSelection(at: index)
         }
     }
-    
+
     func handleItemSelection(at index: Int) {
         didSelectItem?(index)
     }
