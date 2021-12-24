7af09ccdd900d41a82a019f49dbb88edbf7d1e3f
Asmaa <asmaatarek93@gmail.com>
2021-12-24 16:43:51 +0200

    Register and dequeue NewsTableViewCell to NewsTableView

--- BreakingNews/Features/Home/View/HomeViewController.swift
+++ BreakingNews/Features/Home/View/HomeViewController.swift
@@ -18,6 +18,35 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+}
 
+fileprivate extension HomeViewController {
+    
+    func setupView() {
+        setupNewsTableView()
+    }
+    
+    func setupNewsTableView() {
+        newsTableView.delegate = self
+        newsTableView.dataSource = self
+        newsTableView.registerCellNib(cellClass: NewsTableViewCell.self)
+        
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeue() as NewsTableViewCell
+        return cell
     }
 }
