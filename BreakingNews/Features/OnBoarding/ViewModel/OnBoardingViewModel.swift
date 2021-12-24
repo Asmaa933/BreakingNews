dc05e824ee81286f1ba4015b869f1a550815113b
Asmaa <asmaatarek93@gmail.com>
2021-12-24 12:49:27 +0200

    Add OnBoardingViewModelProtocol

--- BreakingNews/Features/OnBoarding/ViewModel/OnBoardingViewModel.swift
+++ BreakingNews/Features/OnBoarding/ViewModel/OnBoardingViewModel.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+protocol OnBoardingViewModelProtocol {
+    var selectedCategoryIndex: Int? { get set}
+    var selectedCountryIndex: Int? { get set}
+    func getCategories() -> [String]
+    func getCountries() -> [String]
+    func startHeadlines()
+}
+
 class OnBoardingViewModel {
     
 }
