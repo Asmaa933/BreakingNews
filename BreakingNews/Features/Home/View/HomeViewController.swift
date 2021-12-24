dd2dfa924d1cce6d3580513f6018f39028bdc09c
Asmaa <asmaatarek93@gmail.com>
2021-12-24 15:27:45 +0200

    Home view design and connect outlets

--- BreakingNews/Features/Home/View/HomeViewController.swift
+++ BreakingNews/Features/Home/View/HomeViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    //MARK: - Outlets
+    
+    @IBOutlet private weak var searchBar: UISearchBar!
+    @IBOutlet private weak var newsTableView: UITableView!
+    
+    //MARK: - View LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
