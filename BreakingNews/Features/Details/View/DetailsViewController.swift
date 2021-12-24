b5944dcd7354f554e1c6c3dc02150dcd116e3fd7
Asmaa <asmaatarek93@gmail.com>
2021-12-24 02:13:16 +0200

    Create Details Feature Structure

---
+++ BreakingNews/Features/Details/View/DetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.swift
+//  BreakingNews
+//
+//  Created by Asmaa Tarek on 24/12/2021.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
