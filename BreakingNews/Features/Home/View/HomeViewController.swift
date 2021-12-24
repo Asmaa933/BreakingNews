2f230da25b30b623a31c016b22ae1e006e7cea78
Asmaa <asmaatarek93@gmail.com>
2021-12-24 02:05:55 +0200

    Create Home Feature Structure

---
+++ BreakingNews/Features/Home/View/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  BreakingNews
+//
+//  Created by Asmaa Tarek on 24/12/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
