b57073a924eb245bc75db19ddea4835ac9757a3e
Asmaa <asmaatarek93@gmail.com>
2021-12-24 12:52:22 +0200

    Add Initializer for OnBoardingViewController

--- BreakingNews/Delegates/SceneDelegate.swift
+++ BreakingNews/Delegates/SceneDelegate.swift
@@ -18,7 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        setRootViewController(viewController: OnBoardingViewController())
+        let onboardingViewModel = OnBoardingViewModel()
+        setRootViewController(viewController: OnBoardingViewController(viewModel: onboardingViewModel))
     }
 }
 
--- BreakingNews/Features/OnBoarding/View/OnBoardingViewController.swift
+++ BreakingNews/Features/OnBoarding/View/OnBoardingViewController.swift
@@ -10,18 +10,30 @@ import UIKit
 class OnBoardingViewController: UIViewController {
 
     //MARK: - Outlets
-    @IBOutlet private weak var countryDropDown: CustomDropDown!
+    @IBOutlet private weak var countriesDropDown: CustomDropDown!
     @IBOutlet private weak var categoriesDropDown: CustomDropDown!
     
+    //MARK: - Variables
+    private var viewModel: OnBoardingViewModelProtocol
+    
+    //MARK: Init
+    init(viewModel: OnBoardingViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     //MARK: - View LifeCycle
-
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
     
     //MARK: - Actions
-
     @IBAction private func startAction(_ sender: BottomButton) {
+        
     }
 }
-
