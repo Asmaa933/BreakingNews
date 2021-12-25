8bce5abd0c9d03131dfaf2ada7a5c1cb267479e6
Asmaa <asmaatarek93@gmail.com>
2021-12-25 12:12:36 +0200

    Add label to drop down when selection

--- BreakingNews/Helpers/CustomUI/CustomDropDown/CustomDropDown.swift
+++ BreakingNews/Helpers/CustomUI/CustomDropDown/CustomDropDown.swift
@@ -12,6 +12,7 @@ class CustomDropDown: UITextField {
     
     var didSelectItem: ((Int) -> Void)? // index
     private let dropMenu = DropDown()
+    private var label = UILabel()
     
     public var dropMenuItems: [String]? {
         get {
@@ -23,26 +24,53 @@ class CustomDropDown: UITextField {
     }
     
     override func awakeFromNib() {
-        superview?.awakeFromNib()
-        setupDropDown()
+        super.awakeFromNib()
+        setupView()
     }
 }
 
 fileprivate extension CustomDropDown {
     
-    func setupDropDown() {
+    func setupView() {
+        setupTextField()
+        setupDropDown()
+        setupLabel()
+    }
+    
+    func setupTextField() {
         self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dropMenuTapped)))
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.borderWidth = 2
+        self.layer.cornerRadius = 10
+    }
+    
+    func setupDropDown() {
         dropMenu.bottomOffset = CGPoint(x: 0, y: (self.bounds.height))
         dropMenu.direction = .any
         dropMenu.anchorView = self.plainView
         dropMenu.selectionAction = {[weak self] (index: Int, item: String) in
             guard let self = self else { return }
-            self.text = item
-            self.handleItemSelection(at: index)
+            self.handleItemSelection(at: index, item: item)
         }
     }
     
-    func handleItemSelection(at index: Int) {
+    func setupLabel() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = placeholder
+        label.textColor = .gray
+        label.isHidden = true
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        self.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.heightAnchor.constraint(equalToConstant: 15),
+            label.bottomAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5)
+        ])
+    }
+    
+    func handleItemSelection(at index: Int, item: String) {
+        self.text = item
+        self.label.isHidden = false
         didSelectItem?(index)
     }
     
