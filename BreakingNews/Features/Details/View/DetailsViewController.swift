e20f2a99b7d3886c894c369c82695839f75b11e9
Asmaa Tarek <asmaatarek93@gmail.com>
2021-12-25 04:28:31 +0200

    Details screen design (#9)
    * Navigate to details when tap on headline
    
    * Details view design
    
    * fix search for category
    
    * Connect outlets and actions

--- BreakingNews/Features/Details/View/DetailsViewController.swift
+++ BreakingNews/Features/Details/View/DetailsViewController.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var authorLabel: UILabel!
+    @IBOutlet private weak var sourceLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    
+    private let viewModel: DetailsViewModelProtocol
+    
+    init(viewModel: DetailsViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
+    @IBAction private func backAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func openArticleAction(_ sender: BottomButton) {
+    }
 }
--- BreakingNews/Helpers/Extensions/UIViewController+Ext.swift
+++ BreakingNews/Helpers/Extensions/UIViewController+Ext.swift
@@ -14,4 +14,12 @@ extension UIViewController {
         navigationController.isNavigationBarHidden = true
         UIApplication.shared.windows.first?.rootViewController = navigationController
     }
+    
+    func push(viewController: UIViewController) {
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    func pop() {
+        navigationController?.popViewController(animated: true)
+    }
 }
