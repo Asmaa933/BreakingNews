040833ced9fe09cea5370e902d4a636c31dafcb1
Asmaa <asmaatarek93@gmail.com>
2021-12-26 02:59:35 +0200

    Test loading, refreshing and get articles

--- BreakingNews/Features/Home/ViewModel/HomeViewModelTests.swift
+++ BreakingNews/Features/Home/ViewModel/HomeViewModelTests.swift
@@ -59,4 +59,62 @@ class HomeViewModelTests: XCTestCase {
         XCTAssertFalse(viewController.isEmpty)
         XCTAssertFalse(viewController.centerLoadingIndicator)
     }
+    
+    func testRefreshData() {
+        let expectationObject = expectation(description: "Testing loading in tableview")
+        CachingManager.shared.saveArticles([FakeArticle().article])
+        let homeProvider = FakeHomeDataSource(shouldReturnError: false, error: nil)
+        let sut = HomeViewModel(userFavorite: userFavorite, dataSource: homeProvider)
+        let viewController = FakeHomeViewController()
+        sut.statePresenter = viewController
+        sut.statePresenter?.render(state: .loading)
+        XCTAssertTrue(viewController.centerLoadingIndicator)
+        sut.fetchArticles(isRefresh: true)
+        XCTAssertTrue(sut.getArticlesCount() == 3)
+        expectationObject.fulfill()
+        XCTAssertTrue(viewController.errorMessage == "")
+        XCTAssertFalse(viewController.isEmpty)
+        XCTAssertFalse(viewController.centerLoadingIndicator)
+        XCTAssertTrue(viewController.isRefresh)
+        XCTAssertFalse(viewController.footerLoadingIndicator)
+        waitForExpectations(timeout: 5)
+    }
+    
+    func testGetArticle() {
+        CachingManager.shared.saveArticles([])
+        let homeProvider = FakeHomeDataSource(shouldReturnError: false, error: nil)
+        let sut = HomeViewModel(userFavorite: userFavorite, dataSource: homeProvider)
+        sut.fetchArticles(isRefresh: false)
+        let article = sut.getArticle(at: 0)
+        XCTAssertEqual(article.author!, "fefe")
+    }
+    
+    func testLoadMoreArticles() {
+        CachingManager.shared.saveArticles([])
+        let homeProvider = FakeHomeDataSource(shouldReturnError: false, error: nil)
+        let sut = HomeViewModel(userFavorite: userFavorite, dataSource: homeProvider)
+        let viewController = FakeHomeViewController()
+        sut.statePresenter = viewController
+        sut.statePresenter?.render(state: .loading)
+        XCTAssertTrue(viewController.centerLoadingIndicator)
+        sut.fetchArticles(isRefresh: false)
+        XCTAssertTrue(sut.getArticlesCount() == 3)
+        XCTAssertFalse(viewController.centerLoadingIndicator)
+        sut.statePresenter?.render(state: .loadingMore)
+        XCTAssertTrue(viewController.footerLoadingIndicator)
+        sut.loadMoreArticles()
+        XCTAssertTrue(sut.getArticlesCount() == 6)
+        XCTAssertFalse(viewController.footerLoadingIndicator)
+        XCTAssertFalse(viewController.isRefresh)
+        XCTAssertFalse(viewController.isEmpty)
+        XCTAssertTrue(viewController.errorMessage.isEmpty)
+
+    }
+    
+    func testGetTitle() {
+        let homeProvider = FakeHomeDataSource(shouldReturnError: false, error: nil)
+        let sut = HomeViewModel(userFavorite: userFavorite, dataSource: homeProvider)
+      let title = sut.getTitle()
+    XCTAssertEqual(title, "Technology news in America")
+    }
 }
